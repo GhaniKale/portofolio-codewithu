b6b8fccefad148a13e49a60b41deaf3483712e2e
Your Name <your.email@example.com>
2026-01-09 05:37:30 +0700

    update

--- src/data/projects.js
+++ src/data/projects.js
@@ -14,8 +14,8 @@ export const projects = [
     description: "Modern e-commerce website for beauty & skincare products.",
     category: "e-commerce",
     image:
-      "https://images.unsplash.com/photo-1522335789203-aabd1fc54bc9?w=800&q=80",
-    vercelUrl: "#",
+      "./asset/skincare.png",
+    vercelUrl: "https://skincare-marketplace.vercel.app/",
   },
   {
     id: 3,
@@ -34,8 +34,8 @@ export const projects = [
     description: "High-converting landing page for startup & SaaS.",
     category: "landing-page",
     image:
-      "https://images.unsplash.com/photo-1498050108023-c5249f4df085?w=800&q=80",
-    vercelUrl: "#",
+      "./asset/startup.png",
+    vercelUrl: "https://startup-lp-orpin.vercel.app/",
   },
   {
     id: 5,
@@ -43,8 +43,8 @@ export const projects = [
     description: "Landing page for product launch & online marketing.",
     category: "landing-page",
     image:
-      "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?w=800&q=80",
-    vercelUrl: "#",
+      "./asset/product.png",
+    vercelUrl: "https://product-lp-gray.vercel.app/",
   },
   {
     id: 6,
@@ -63,7 +63,7 @@ export const projects = [
     category: "company-profile",
     image:
       "https://images.unsplash.com/photo-1460925895917-afdab827c52f?w=800&q=80",
-    vercelUrl: "#",
+    vercelUrl: "https://cp-agency-zeta.vercel.app/",
   },
   {
     id: 8,
@@ -72,7 +72,7 @@ export const projects = [
     category: "company-profile",
     image:
       "https://images.unsplash.com/photo-1521737604893-d14cc237f11d?w=800&q=80",
-    vercelUrl: "#",
+    vercelUrl: "https://corporate-business-omqx-git-main-ghanikales-projects.vercel.app/",
   },
   {
     id: 9,
@@ -91,7 +91,7 @@ export const projects = [
     category: "education",
     image:
       "https://images.unsplash.com/photo-1503676260728-1c00da094a0b?w=800&q=80",
-    vercelUrl: "#",
+    vercelUrl: "https://course-platform-gray.vercel.app/",
   },
   {
     id: 11,
@@ -100,10 +100,19 @@ export const projects = [
     category: "education",
     image:
       "https://images.unsplash.com/photo-1524995997946-a1c2e315a42f?w=800&q=80",
-    vercelUrl: "#",
+    vercelUrl: "https://school-web-indol.vercel.app/",
+  },
+    {
+    id: 12,
+    title: "Online Training Platform",
+    description: "Website for online training & certification.",
+    category: "education",
+    image:
+      "https://images.unsplash.com/photo-1524995997946-a1c2e315a42f?w=800&q=80",
+    vercelUrl: "https://training-platform-rouge.vercel.app//",
   },
   {
-    id: 12,
+    id: 13,
     title: "Pomodoro Todo",
     description:
       "An educational productivity app using the Pomodoro technique for focused learning.",
@@ -114,34 +123,34 @@ export const projects = [
 
   /* ==================== RENTAL ===================== */
   {
-    id: 13,
+    id: 14,
     title: "Car Rental Website",
     description: "Car rental website with booking & contact feature.",
     category: "rental",
     image:
       "https://images.unsplash.com/photo-1502877338535-766e1452684a?w=800&q=80",
-    vercelUrl: "#",
+    vercelUrl: "https://rent-car-topaz.vercel.app/",
   },
   {
-    id: 14,
+    id: 15,
     title: "Motorbike Rental",
     description: "Motorbike rental website with pricing & contact.",
     category: "rental",
     image:
       "https://images.unsplash.com/photo-1511919884226-fd3cad34687c?w=800&q=80",
-    vercelUrl: "#",
+    vercelUrl: "https://rent-motorbike-gray.vercel.app/"
   },
   {
-    id: 15,
+    id: 16,
     title: "Property Rental Website",
     description: "Rental website for houses, apartments & rooms.",
     category: "rental",
     image:
       "https://images.unsplash.com/photo-1568605114967-8130f3a36994?w=800&q=80",
-    vercelUrl: "#",
+    vercelUrl: "https://rent-property-amber.vercel.app/",
   },
   {
-    id: 15,
+    id: 17,
     title: "Produktivity",
     description:
       "An educational productivity app with activity and habit tracking features.",
